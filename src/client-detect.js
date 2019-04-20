4b9e40699f58b6b789e60d5c0565de95817f6510
Catia Gomes <catia_gomes82@hotmail.com>
2019-04-20 17:37:14 +0100

    detect modern browsers & IE11, fully tested

--- src/client-detect.js
+++ src/client-detect.js
@@ -1,18 +1,30 @@
 (function (window) {
     window.client = new function Client() {
         var winNav = window.navigator,
-            isOpera = winNav.userAgent.indexOf("OPR") > -1,
-            isEdge = winNav.userAgent.indexOf("Edge") > -1,
-            isSafari = winNav.userAgent.indexOf("Safari") > -1,
+            isOpera = winNav.userAgent.indexOf('OPR') > -1,
+            isEdge = winNav.userAgent.indexOf('Edge') > -1,
+            isSafari = winNav.userAgent.indexOf('Safari') > -1 && !isEdge && !isOpera,
+            isIosChrome = winNav.userAgent.indexOf('CriOS') > -1,
+            isFirefox = winNav.userAgent.indexOf('Firefox') > -1,
+            isIe11 = (navigator.userAgent.indexOf('Trident/') > -1 && navigator.userAgent.indexOf('rv:11.0') > -1),
+            isOldIe = winNav.userAgent.indexOf('MSIE') > -1,
+            isIe = isOldIe || isIe11,
+            isChrome = (winNav.userAgent.indexOf('Chrome') > -1 && !isEdge && !isOpera) || isIosChrome,
             clientDetect = {
-                isChrome: winNav.userAgent.indexOf("Chrome") > -1 && !isEdge && !isOpera,
+                isChrome: isChrome,
                 isOpera: isOpera,
-                isEdge: isEdge,
-                isSafari: isSafari
+                isEdge: isEdge && !isSafari,
+                isSafari: isSafari && !isChrome,
+                isFirefox: isFirefox,
+                isIE: isIe,
+                isIE11: isIe11,
+                isOldIE: isOldIe
             },
         
             init = function () {
                 window.clientDetect = clientDetect;
+                alert(winNav.userAgent)
+                alert('IE ' + clientDetect.isIE + ';chrome ' + clientDetect.isChrome + ';safari ' + clientDetect.isSafari + ';firefox ' + clientDetect.isFirefox + ';edge ' + clientDetect.isEdge)
                 console.log(clientDetect);            
             }
 
